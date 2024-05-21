24a6d4d3192412962fd7783fb909aeadcd399c5b
Chirag T <chirag.tatariya@rapidops.com>
2024-05-21 17:42:11 +0530

    - rebase with master and used old inappwebview version

--- lib/src/widgets/html_editor_widget_mobile.dart
+++ lib/src/widgets/html_editor_widget_mobile.dart
@@ -143,14 +143,17 @@ class _HtmlEditorWidgetMobileState extends State<HtmlEditorWidget> {
                           }
                         });
                   },
-                  initialSettings: InAppWebViewSettings(
-                    javaScriptEnabled: true,
-                    transparentBackground: true,
-                    useShouldOverrideUrlLoading: true,
-                    useHybridComposition: widget.htmlEditorOptions
-                        .androidUseHybridComposition,
-                    loadWithOverviewMode: true,
-                  ),
+                  initialOptions: InAppWebViewGroupOptions(
+                      crossPlatform: InAppWebViewOptions(
+                        javaScriptEnabled: true,
+                        transparentBackground: true,
+                        useShouldOverrideUrlLoading: true,
+                      ),
+                      android: AndroidInAppWebViewOptions(
+                        useHybridComposition: widget
+                            .htmlEditorOptions.androidUseHybridComposition,
+                        loadWithOverviewMode: true,
+                      )),
                   initialUserScripts:
                       widget.htmlEditorOptions.mobileInitialScripts
                           as UnmodifiableListView<UserScript>?,
